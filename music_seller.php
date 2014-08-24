77377e45a3d1e326fa7e607020a0220ce6575a65
motov.net <motov.net@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-24 12:34:32 +0000

    Added Add all to cart feature for paid version and fixed a bug in both versions regarding the cart icon.
    git-svn-id: https://plugins.svn.wordpress.org/music-seller/tags/2.4@971690 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ music_seller.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: Music Seller
+Plugin URI: http://shopfiles.com/
+Description: A powerful tool for selling music with WordPress
+Author: Deian Motov
+Author URI:http://shopfiles.com
+Version: 2.4
+License: GPLv2
+*/
+
+include_once('functions.php');
+
+include_once('music_seller_options.php');
+
+
+/**
+ * Adds a box to the main column on the Post and Page edit screens.
+ */
+
+
+
+add_action( 'add_meta_boxes', 'music_seller_add_meta_box' );
+add_action("manage_posts_custom_column", "music_seller_order_custom_columns");
+add_filter("manage_edit-music_seller_order_columns", "music_seller_order_columns");
+add_action('admin_head', 'hide_that_stuff');
+add_action('save_post', 'music_seller_save_custom_meta_data'); 
+add_action('post_edit_form_tag', 'music_seller_update_edit_form');
+add_action( 'init', 'music_seller_orders_post_type' );
+add_action('plugins_loaded', 'music_seller_ipn');
+add_action( 'init', 'music_seller_process_download', 100 );
+
+
+
+?>
